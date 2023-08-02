95b44bccf12bb1632c3b04594a7290ae9da1dba4
tarikul_islam_anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-08-02 16:54:19 +0600

    fixed maximum stack bug and added a loading animation.

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,55 @@
 "use client";
 
-import Feeds from "@/components/home/feeds";
 import LandingPage from "@/components/home/landing-page";
-import { Navbar } from "@/components/shared";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import Image from "next/image";
 import { redirect } from "next/navigation";
-import { userInfo } from "os";
 import { useEffect } from "react";
 
+const Loader = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="animate-spin"
+    >
+      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+    </svg>
+  );
+};
 
 export default function Home() {
   const { user, isLoading } = useUser();
 
   useEffect(() => {
-      if(user) {
-        redirect('/feed');
-      }
-  }, [user])
+    if (user) {
+      redirect("/feed");
+    }
+  }, []);
+
   if (isLoading) {
-    return <>
-        <div className="flex justify-center align bg-slate-500">
-          <Image src={'https://cdn.auth0.com/blog/hello-auth0/loader.svg'} alt="Loading..." height={100} width={100} />
+    return (
+      <>
+        <div className="grid place-content-center h-screen">
+          <div className="flex space-x-2 items-center">
+            <Loader /> <span>Loading...</span>
+          </div>
         </div>
       </>
+    );
   }
-  return (!user && !isLoading) &&
-  <>
-    <LandingPage />
-  </>;
+  return (
+    !user &&
+    !isLoading && (
+      <div className="grid place-content-center h-screen">
+        <LandingPage />
+      </div>
+    )
+  );
 }
